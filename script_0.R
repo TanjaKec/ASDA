42c8261c75f100d5257cce85bd24ecd3719859ff
your name <your@email.com>
2020-11-11 19:56:20 +0100

    script: packages to install

---
+++ script_0.R
@@ -0,0 +1,23 @@
+# Packages required for the Case Study on how to use R 
+
+# for reading data
+install.packages(readxl) 
+install.packages(httr)
+# for data wrangling 
+install.packages(tidyverse)
+install.packages(lubridate)
+install.packages(dplyr)
+# for data visualisation
+install.packages(cowplot)
+install.packages(scales)
+install.packages(leaflet)
+install.packages(plotly)
+install.packages(ggplot2)
+install.packages(sf)
+install.packages(tmap)
+install.packages(tmaptools)
+# for working with DB
+install.packages(RODBC)
+install.packages(DBI)
+install.packages(dbplyr)
+install.packages(odbc)
