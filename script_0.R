caedd85adcc4b18a13c05524a1ffcc156de54874
your name <your@email.com>
2020-11-17 11:01:08 +0100

    script: packages to install

--- script_0.R
+++ script_0.R
@@ -6,6 +6,7 @@ install.packages("httr")
 install.packages("tidyverse")
 install.packages("lubridate") 
 install.packages("dplyr")
+
 # packages for data visualisation
 install.packages("cowplot")
 install.packages("scales")
@@ -15,8 +16,22 @@ install.packages("ggplot2")
 install.packages("sf")
 install.packages("tmap")
 install.packages("tmaptools")
+
 # packages for working with DB
 install.packages("RODBC")
 install.packages("DBI")
 install.packages("dbplyr")
 install.packages("odbc")
+
+# for data objects in R that can be rendered as HTML tables 
+install.packages("DT")
+
+# packages needed for 'Introduction To R'
+# https://introtor.netlify.app/
+install.packages("gapminder") # data available at Gapminder.org
+install.packages("ggthemes") # extra themes, geoms, and scales for 'ggplot2'
+install.packages("forcats") # tools for working with categorical/factor variables
+install.packages("knitr") # for dynamic report generation in R
+install.packages("rmarkdown") # convert R Markdown documents into a variety of formats
+install.packages("rticles") # a suite of custom R Markdown formats and templates for authoring journal articles and conference submissions
+install.packages("here") # locate the files relative to your project root
